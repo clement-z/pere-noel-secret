0daf5b6239ebfd23a815ccd8002a27fc84018689
Clément Zrounba <6691770+clement-z@users.noreply.github.com>
2020-12-05 14:24:17 +0100

    update send_mails

--- send_mails.py
+++ send_mails.py
@@ -9,27 +9,35 @@ mail_subject_fmt = 'Père Noël secret de la bande hétérogène'
 mail_body_fmt = '''
 Salut {gifter.name},
 
-Pour le Père Noël secret, tu as été sélectionné(e) pour envoyer un cadeau à
-{gifted.name} !
+Pour Noël, tu as été sélectionné(e) pour envoyer un cadeau à {gifted.name} !
 
 Ses coordonnées sont les suivantes:
-    {gifted.name}
-    {gifted.address}
-    {gifted.tel}
-    {gifted.email}
+    - {gifted.name}
+    - {gifted.address}
+    - {gifted.tel}
+    - {gifted.email}
 
 Dans l'idée, il faudrait de préférence un cadeau personnel, fait à la main ou à
-moins de 10€ (comme ça, tu dois forcément y mettre un peu du tiens ou ce sera
-un cadeau nul !). Essaie de t'y prendre un peu en amont pour le livrer à temps !
+moins de 10€ (comme ça, tu dois forcément y mettre un peu du tien ou ce sera un
+cadeau nul !).
+
+Essaie de t'y prendre un peu en amont pour le livrer à temps !
 
 Bisous et en te souhaitant un joyeux noël,
-Python et sendmail
+Le Père Noël
 '''
 
 def main(do_it=False):
     with open('results.pkl', 'rb') as f:
         results = pickle.load(f)
 
+    if not do_it:
+        print('This only send data if you give it --ok')
+        print('Would send the following mails:')
+        print('')
+
+    n = len(results)
+    i = 0
     for result in results:
         gifter = result[0]
         gifted = result[1]
@@ -40,12 +48,19 @@ def main(do_it=False):
         msg['From'] = 'Le Père Noël <pere.noel+donotreply@zrounba.fr>'
         msg['To'] = f'{gifter.name} <{gifter.email}>'
         msg['Subject'] = mail_subject_fmt.format()
-        print(msg)
         
         if do_it:
             p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
             p.communicate(msg.as_bytes())
+            i += 1
+            print(f'{i}/{n} mails sent.')
+        else:
+            print(msg)
+
 
 if __name__ == '__main__':
-    main(do_it=True)
-    #main()
+    import sys
+    if len(sys.argv) == 2 and sys.argv[1] == '--ok':
+        main(do_it=True)
+    else:
+        main()
