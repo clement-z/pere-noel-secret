50d32b2e88993056012089daaf16cde6e7d33432
Clément Zrounba <6691770+clement-z@users.noreply.github.com>
2020-12-05 16:26:15 +0100

    fix send_mails

--- send_mails.py
+++ send_mails.py
@@ -4,12 +4,14 @@ import os
 import pickle
 from generate_list import SimplePerson
 from email.message import EmailMessage
+from email.generator import BytesGenerator
+from email.policy import EmailPolicy
 from subprocess import Popen, PIPE
 
 domain = 'zrounba.fr'
 sender_name = 'Le Père Joël'
 sender_email = 'pere.noel+donotreply@'+domain
-mail_subject_fmt = 'Père Noël secret de la bande hétérogène'
+mail_subject_fmt = 'Père Noël hétérogène'
 mail_body_fmt = '''Bonjour {gifter.name},
 
 Pour Noël, tu as été sélectionné(e) pour envoyer un cadeau à {gifted.name} !
@@ -45,21 +47,29 @@ def main(do_it=False):
         gifted = result[1]
 
         #mail_body = mail_body_fmt.format(gifter=result[0], gifted=result[1])
+        #mail_from = f'{sender_email}'
+        #mail_to = f'{gifter.email}'
         mail_from = f'{sender_name} <{sender_email}>'
         mail_to = f'{gifter.name} <{gifter.email}>'
         mail_subject = mail_subject_fmt.format()
         mail_body = mail_body_fmt.format(gifter=gifter, gifted=gifted)
 
         msg = EmailMessage()
-        msg.set_charset('utf-8')
         msg['From'] = mail_from
         msg['To'] = mail_to
         msg['Subject'] = mail_subject
         msg.set_payload(mail_body, charset='utf-8')
         
         if do_it:
-            p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-            p.communicate(msg.as_bytes())
+            #p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+            #p.communicate(msg.as_bytes())
+            
+            p = Popen(['sendmail', '-F', '-oi', gifter.email], stdin=PIPE)
+            g = BytesGenerator(p.stdin, policy=msg.policy.clone(linesep='\r\n'))
+            g.flatten(msg)
+            p.stdin.close()
+            rc = p.wait()
+
             i += 1
             print(f'{i}/{n} mails sent.')
         else:
