ff95df24b164836525da09ac59ced74404cb5995
Clément Zrounba <6691770+clement-z@users.noreply.github.com>
2020-12-06 16:06:09 +0100

    improve rule stuff and logging

--- generate_list.py
+++ generate_list.py
@@ -57,15 +57,31 @@ def main(coords_file='coords.csv', results_file='results.pkl', blacklist_file='b
     if os.path.exists(blacklist_file):
         with open(blacklist_file, 'r') as f:
             for line in f:
-                ids = [int(x) for x in line.split(' ')]
-                if len(ids) != 2 or ids[0] == ids[1]:
-                    print(f'[ERROR] invalid rule found (need only two different integers separated by spaces): {line:r}')
+                line = line.rstrip()
+                try:
+                    ids = [int(x) for x in line.split(' ')]
+                except ValueError:
+                    # rule contains something not convertivle to an integer
+                    print(f'[ERROR] invalid rule found (need two different integers separated by spaces): {line!r}')
                     return 1
+
+                # same numbers
+                if ids[0] == ids[1]:
+                    print(f'[ERROR] invalid rule found (need different numbers): {line!r}')
+                    return 1
+
+                # not 2 numbers
+                if len(ids) != 2:
+                    print(f'[ERROR] invalid rule found (need exactly two numbers but found {len(ids)}): {line!r}')
+                    return 1
+
+                # numbers higher than total head count
                 if ids[0] >= len(persons) or ids[1] >= len(persons):
-                    print(f'[ERROR] rule refers to persons #{ids[0]} and #{ids[1]} but only {len(persons)} specified in coords file: {line:r}')
+                    print(f'[ERROR] rule refers to persons #{ids[0]} and #{ids[1]} but only {len(persons)} specified in coords file: {line!r}')
                     return 1
                 rules.append(set(ids))
-        print(f'[INFO] Found {len(rules)} valid rules in {blacklist_file}:')
+
+        print(f'[INFO] Found {len(rules)} rules in {blacklist_file}:')
         for rule in rules:
             print(f'[INFO]\t- {persons[list(rule)[0]].name} will not be matched with {persons[list(rule)[1]].name}')
 
@@ -79,6 +95,8 @@ def main(coords_file='coords.csv', results_file='results.pkl', blacklist_file='b
                 valid_output = False
                 break
 
+    print('[INFO] List generated')
+
     if verbose:
         print('[DEBUG] Results:')
         for (i,j) in zip(id_from, id_to):
@@ -90,6 +108,8 @@ def main(coords_file='coords.csv', results_file='results.pkl', blacklist_file='b
 
     with open(results_file, 'wb') as f:
         pickle.dump(results, f)
+
+    print(f'[INFO] Results saved in: {results_file}')
 
 if __name__ == '__main__':
     main()
